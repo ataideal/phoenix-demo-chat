d84a409adfe281c851ecdb3ba328d2a985ccf7f7
Ataide Neto <ataide.neto31@gmail.com>
2018-02-19 23:26:11 -0300

    Tracking users online

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -14,7 +14,8 @@ let channel = socket.channel("chat:lobby", {})
 let messageInput = document.querySelector("#message-input")
 let usernameInput = document.querySelector("#username")
 let messagesContainer = document.querySelector("#messages")
-
+let onlineLabel = document.querySelector("#online")
+let online = 0
 messageInput.addEventListener("keypress", event => {
   if(event.keyCode === 13){
     channel.push("new_msg", {text: messageInput.value, username: "@"+usernameInput.value})
@@ -28,6 +29,26 @@ channel.on("new_msg", payload => {
   messagesContainer.appendChild(messageItem)
 })
 
+channel.on("presence_state", payload => {
+  updateOnlineCount(payload.presence_list)
+})
+
+function updateOnlineCount(presence_list) {
+  enterOrLeave (online,Object.keys(presence_list).length)
+  online = Object.keys(presence_list).length
+  onlineLabel.innerHTML = "Users online: "+online
+}
+function enterOrLeave(online,newOnline){
+  let messageItem = document.createElement("li");
+  if (online < newOnline) {
+    messageItem.innerHTML = `<b>Um tanga entrou no chat!`
+    messagesContainer.appendChild(messageItem)
+  }else if (online > newOnline){
+    messageItem.innerHTML = `<b>Um tanga saiu do chat!`
+    messagesContainer.appendChild(messageItem)
+  }
+}
+
 function renderComments (comments){
   const renderedComments = comments.map(comment => {
     let messageItem = document.createElement("li");
@@ -36,11 +57,22 @@ function renderComments (comments){
   });
 }
 
+function joinChat (comments){
+  let messageItem = document.createElement("li");
+  messageItem.innerHTML = `<b>Voce entrou no chat!`
+  messagesContainer.appendChild(messageItem)
+}
+
 channel.join()
   .receive("ok", resp => {
         console.log("Joined the channel")
         messagesContainer.innerHTML = "";
-        renderComments (resp.comments)})
+        renderComments (resp.comments)
+        //joinChat()
+      })
+
   .receive("error", resp => { console.log("Unable to join", resp) })
+
+
 
 export default socket
--- lib/chatdemo_web/channels/chat_channel.ex
+++ lib/chatdemo_web/channels/chat_channel.ex
@@ -4,8 +4,8 @@ defmodule ChatdemoWeb.ChatChannel do
    alias ChatdemoWeb.Presence
    require IEx
    def join("chat:lobby", _message, socket) do
-    send(self, :after_join)
-    {:ok, %{comments: Chat.list_comments}, assign(socket,:user_id,1)}
+    send(self(), :after_join)
+    {:ok, %{comments: Chat.list_comments}, assign(socket,:user_id,Enum.random(1..1000))}
   end
 
   def join("chat:" <> _private_room_id, _params, _socket) do
@@ -13,9 +13,6 @@ defmodule ChatdemoWeb.ChatChannel do
   end
 
   def handle_in("new_msg", %{"text" => text, "username"=> username}, socket) do
-    IO.inspect("++")
-    IO.inspect(Presence.list(socket))
-    IO.inspect("++")
     case Chat.create_comment(%{text: text,username: username}) do
       {:ok, comment} ->
         broadcast! socket, "new_msg", %{comment: comment}
@@ -26,11 +23,22 @@ defmodule ChatdemoWeb.ChatChannel do
 
   end
 
+  def terminate(msg, socket) do
+    Presence.untrack(socket, socket.assigns.user_id)
+    broadcast_from socket, "presence_state", %{presence_list: Presence.list(socket)}
+    {:noreply, socket}
+  end
+
   def handle_info(:after_join, socket) do
-    push socket, "presence_state", Presence.list(socket)
-    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+    IO.inspect("++")
+    IO.inspect(Presence.list(socket))
+    IO.inspect("++")
+    IO.inspect(socket.assigns.user_id)
+    IO.inspect("++")
+    {:ok, _socket} = Presence.track(socket, socket.assigns.user_id, %{
       online_at: inspect(System.system_time(:seconds))
     })
+    broadcast socket, "presence_state", %{presence_list: Presence.list(socket)}
     {:noreply, socket}
   end
 
